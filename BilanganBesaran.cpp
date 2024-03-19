d4676cde6faa16d266f436e1fea2346411106ded
Mihaly24 <fairuzk20@gmail.com>
2024-03-19 18:26:48 +0700

    Menambah projek kedua

---
+++ BilanganBesaran.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int bilangan;
+    srand(time(0));
+    bilangan = rand() % 10;
+    while (bilangan > 4)
+    {
+        cout << "Bilangan lebih dari 4" << endl;
+        bilangan = rand() % 10;
+        cout << "Bilangannya = " << bilangan << endl;
+    }
+
+    cout << "bilangan terakhir = " << bilangan << endl;
+}
